2b6b0ed134d55d1f99da1bce63fd5e6354014c09
nonderbe <niels.onderbeke@gmail.com>
2025-05-09 22:33:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './tailwind.css';
-import './i18n/i18n';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
