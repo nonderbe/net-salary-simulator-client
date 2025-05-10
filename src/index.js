f60aa2a1fb0d5aaa440440d93ada8d06a282272d
nonderbe <niels.onderbeke@gmail.com>
2025-05-10 11:11:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import enTranslation from './i18n/en.json';
+import nlTranslation from './i18n/nl.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      nl: { translation: nlTranslation },
+    },
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
